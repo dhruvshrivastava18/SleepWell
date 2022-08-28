1d4e1c48eb410e02f27b65a179fc0623e377550f
DHRUV SHRIVASTAVA <dhruvshrivastava18@gmail.com>
2022-08-28 22:58:00 +0530

    Initial Commit

---
+++ SleepWell/SleepWellApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepWellApp.swift
+//  SleepWell
+//
+//  Created by Dhruv Shrivastava on 28/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SleepWellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
